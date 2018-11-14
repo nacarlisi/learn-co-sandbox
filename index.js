979c6cfa074086a95f1c3adf0bf8aefdd10ac654
Nicholas Carlisi <nacarlisi@gmail.com>
2018-11-14 04:00:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var name = "Nick"
-var height = "74"
+var height = 74
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height)+1)
+console.log(parseInt(height) + 1 )
